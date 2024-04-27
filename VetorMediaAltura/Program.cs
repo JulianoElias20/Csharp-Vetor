e47c16a6285c43ecab5f575de81c842ffce320fb
Juliano Elias <julianorelias94@hotmail.com>
2024-04-27 01:00:42 -0300

    Calculando a média da altura através de vetores

---
+++ VetorMediaAltura/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Globalization;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Quantas pessoas você deseja calcular a média da altura: ");
+            int n = int.Parse(Console.ReadLine());
+
+            double[] vect = new double[n];// Neste momentos instanciamos as caixinhas do vetor, que será onde ficará guardado os dados
+
+            for (int i = 0; i < n; i++)
+            {
+                Console.WriteLine("Digite as alturas ");
+                vect[i] = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+
+            }
+
+            double soma = 0;
+
+            for (int i = 0; i < n; i++)
+            {
+                soma += vect[i];
+            }
+
+            double media = soma / n;
+            Console.WriteLine("A média é " + media.ToString("F2", CultureInfo.InvariantCulture));
+        }
+    }
+}
